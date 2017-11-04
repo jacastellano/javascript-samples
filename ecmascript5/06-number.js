a3dbf663ce7f01c89c703e4f4e663ce3ae88ea7a
jacastellano <jacastellanoa@gmail.com>
2017-11-04 17:35:07 +0100

    Adding Number test

---
+++ ecmascript5/06-number.js
@@ -0,0 +1,12 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+  Description: EcmaScript 5 - Number test
+  Date: 2017/11/04
+  Author: José Antonio Castellano Ardila
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Number examples
+console.log(Number('3'));
+console.log(Number('0XAFED00'));
+console.log(Number(4546546.987654321));
+console.log(Number(1/3));
+console.log(Number('3d'));
