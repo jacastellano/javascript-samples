d181dbd0a10ae269122ce8ec436dc4f1a499f7b1
jacastellano <jacastellanoa@gmail.com>
2017-11-09 21:20:36 +0100

    Updating emascript 6 new features - extends

--- ecmascript6/01-new-features.js
+++ ecmascript6/01-new-features.js
@@ -55,3 +55,47 @@ p.printName();
 
 // print Location
 p.printLocation();
+
+
+class Vehicle {
+    constructor(mark, model, color) {
+        this.mark = mark;
+        this.model = model;
+        this.color = color;
+    }
+
+    toString() {
+        return "Mark: " + this.mark + ", Model: " + this.model + ", Color: " + this.color;
+    }
+}
+
+class Car extends Vehicle {
+    constructor(mark, model, color, licensePlate) {
+        super(mark, model, color);
+        this.licensePlate = licensePlate;
+    }
+
+    toString() {
+        return "License Plate: " + this.licensePlate + ", " + super.toString();
+    }
+}
+
+class Bike extends Vehicle {
+    constructor(mark, model, color, size) {
+        super(mark, model, color);
+        this.size = size;
+    }
+
+    toString() {
+        return super.toString() + ", Size: " + this.size;
+    }
+}
+
+const myVehicle = new Vehicle("Seat", "León", "Red");
+console.log(myVehicle.toString());
+
+const myCar = new Car("Seat", "León", "Red", "4567HNM");
+console.log(myCar.toString());
+
+const myBike = new Bike("Merida", "Scultura Evo", "Blue", "S/M");
+console.log(myBike.toString());
